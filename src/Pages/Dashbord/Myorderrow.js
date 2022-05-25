3e183ea186fdbf0156e16c3912e7730583340ee1
naim <naimctg2000@gmail.com>
2022-05-25 15:39:21 +0600

    'orderorw'

--- src/Pages/Dashbord/Myorderrow.js
+++ src/Pages/Dashbord/Myorderrow.js
@@ -1,34 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 
-const Myorderrow = ({order,refetch,setDeletingproduct,index}) => {
-    const{client,price,name,orderQunatity,clientemail,_id,transactionId}=order;
+const Myorderrow = ({ order, refetch, setDeletingproduct, index }) => {
+    const { client, price, name, orderQunatity, clientemail, _id, transactionId } = order;
     const { id } = useParams();
-    const [product, setProduct] = useState({ });
-    const navigate=useNavigate();
+    const [product, setProduct] = useState({});
+    const navigate = useNavigate();
     // useEffect(() => {
     //     fetch(`https://pacific-harbor-82020.herokuapp.com/product/${id}`)
     //         .then(res => res.json())
     //         .then(data => setProduct(data))
     // }, [])
-    const handleClick=(_id)=>{
+    const handleClick = (_id) => {
         navigate(`/order/${_id}`)
     }
- 
+
     return (
-        
+
         <tr>
             <td className='py-2 fs-5'>{index + 1}</td>
-        
+
             <td className='py-2 fs-5'> {name}</td>
-           
+
             <td className='py-2 fs-5'> {price}</td>
-            
-          
-            <td>{(order.price && !order.paid) && <button className='btn btn-secondary' onClick={()=>handleClick(_id)}>Pay</button>}
-               {(order.price && order.paid) && <div><button className='btn btn-success'>paid</button> <p>Transaction Id: <span className='text-warning'>:{order.transactionId}</span></p></div> } </td>
-            <td className='fs-4'><button onClick={() => setDeletingproduct(order)} data-bs-toggle="modal" data-bs-target="#orderdeleteModal"  className='btn btn-danger'>Delete</button></td>
-            
+
+
+            <td>{(order.price && !order.paid) && <button className='btn btn-secondary' onClick={() => handleClick(_id)}>Pay</button>}
+                {(order.price && order.paid) && <div><button className='btn btn-success'>paid</button> <p>Transaction Id: <span className='text-warning'>:{order.transactionId}</span></p></div>} </td>
+            <td className='fs-4'><button onClick={() => setDeletingproduct(order)} data-bs-toggle="modal" data-bs-target="#orderdeleteModal" className='btn btn-danger'>Delete</button></td>
+
 
         </tr>
     );
