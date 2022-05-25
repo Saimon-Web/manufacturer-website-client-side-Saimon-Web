2d0d6bde0afca4a00d97c55203e9fc65f4eb2f17
naim <naimctg2000@gmail.com>
2022-05-25 15:41:06 +0600

    'portfolio'

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Portfolio = () => {
     return (
         <div>
-          
-             <h4>Naim Arafat</h4> <br />
-             <h5>Email:naimctg2000@gmail.com</h5> <br />
-             <h6>Department of Mathematics(Honurs 3rd year) ,Govt city college,Chittagong</h6> <br />
-<p>I am expert in html,css,bootstrap,bootstrap components,tailwind css,tailwind css component,javascript,React js,Mongodb,express js,node js,Rest api</p>
+
+            <h4>Naim Arafat</h4> <br />
+            <h5>Email:naimctg2000@gmail.com</h5> <br />
+            <h6>Department of Mathematics(Honurs 3rd year) ,Govt city college,Chittagong</h6> <br />
+            <p>I am expert in html,css,bootstrap,bootstrap components,tailwind css,tailwind css component,javascript,React js,Mongodb,express js,node js,Rest api</p>
 
             <h5>My Project Link</h5>
             <a href="https://car-delarship-project.web.app/">https://car-delarship-project.web.app/</a> <br />
