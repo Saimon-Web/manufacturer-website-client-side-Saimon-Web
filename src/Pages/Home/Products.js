fcd0e5517bc1cdab5462ec23f0c2a9275d04a886
naim <naimctg2000@gmail.com>
2022-05-25 15:43:07 +0600

    'partsomeedit'

--- src/Pages/Home/Products.js
+++ src/Pages/Home/Products.js
@@ -6,28 +6,28 @@ import Product from './Product';
 
 const Products = () => {
 
-    const {data:products,isLoading,refetch}=useQuery('/product',()=> fetch('https://pacific-harbor-82020.herokuapp.com/product').then(res=>res.json()))
-    if(isLoading){
+    const { data: products, isLoading, refetch } = useQuery('/product', () => fetch('https://pacific-harbor-82020.herokuapp.com/product').then(res => res.json()))
+    if (isLoading) {
         return <Loading></Loading>
     }
     return (
         <div className='mt-5'>
-           <h2 className='text-warning fs-1 fw-bold'>Parts</h2>
-         <div className="container">
-             <div className="row">
-             {
-               products.map(product => <Product
-               key={product._id}
-               product={product}
-               ></Product>)
-           }
-       
-               
-             </div>
-            
+            <h2 className='text-warning fs-1 fw-bold'>Parts</h2>
+            <div className="container">
+                <div className="row">
+                    {
+                        products.map(product => <Product
+                            key={product._id}
+                            product={product}
+                        ></Product>)
+                    }
 
-         </div>
-            
+
+                </div>
+
+
+            </div>
+
         </div>
     );
 };
