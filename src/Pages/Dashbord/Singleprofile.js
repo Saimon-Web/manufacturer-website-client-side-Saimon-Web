53930c755880d0cb3c22ebdb014598bef345a47d
naim <naimctg2000@gmail.com>
2022-05-26 17:10:25 +0600

    'dfds'

--- src/Pages/Dashbord/Singleprofile.js
+++ src/Pages/Dashbord/Singleprofile.js
@@ -5,8 +5,8 @@ import auth from '../../firebase.init';
 import Loading from '../Loading';
 
 const Singleprofile = () => {
-    const [user]=useAuthState(auth)
-    const { data:singles, isLoading, refetch } = useQuery(['userprofile', user], () => fetch(`https://pacific-harbor-82020.herokuapp.com/singleprofile?useremail=${user?.email}`, {
+    const [user] = useAuthState(auth)
+    const { data: singles, isLoading, refetch } = useQuery(['userprofile', user], () => fetch(`https://pacific-harbor-82020.herokuapp.com/singleprofile?useremail=${user?.email}`, {
         method: "GET",
         headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
@@ -18,15 +18,45 @@ const Singleprofile = () => {
     }
 
     return (
-        <div className='border shadow py-5'>
-            <h2>single : {singles.length}</h2>
-        {
-            singles.map(single=> <>
-                 <p>Education:{single.education}</p>
-              <p>Phone:{single.phone}</p>
-              <p>Linkdinprofile{single.linkdinprofile}</p>
-            </>)
-        }
+        <div className='border shadow py-5 px-5'>
+
+            {
+                singles.map(single => <>
+                    <p>Education:{single.education}</p>
+                    <p>Phone:{single.phone}</p>
+                    <p>Linkdinprofile{single.linkdinprofile}</p>
+                    <button>Update</button>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@getbootstrap">Open modal for @getbootstrap</button>
+
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form>
+                                        <div class="mb-3">
+                                            <label for="recipient-name" class="col-form-label">Recipient:</label>
+                                            <input type="text" class="form-control" id="recipient-name" />
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="message-text" class="col-form-label">Message:</label>
+                                            <textarea class="form-control" id="message-text"></textarea>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Send message</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </>)
+            }
+
         </div>
     );
 };
